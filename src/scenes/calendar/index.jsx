a294b8d222d86005a4fbe23229c990e5d074e683
magige24 <davidmagige1998@gmail.com>
2025-01-31 10:03:39 +0300

    Initial commit

---
+++ src/scenes/calendar/index.jsx
@@ -0,0 +1,14 @@
+import {nuseState } from "react";
+import FullCalendar, { formDate } from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/timegrid';
+import timeGridplugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import listPlugin from '@fullcalendar/list';
+import {
+    Box,
+    List,
+    ListItem,
+    Listitemtext,
+    Typography,
+    usetheme,
+} from "@mui/material";
