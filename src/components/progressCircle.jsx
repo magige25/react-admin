155db3c64aa24451bdc58a593599e4ae268892e9
magige24 <davidmagige1998@gmail.com>
2025-02-02 18:42:59 +0300

    update my react-admin app

---
+++ src/components/progressCircle.jsx
@@ -0,0 +1,28 @@
+import { Box, useTheme } from "@mui/material";
+import { tokens } from "../theme";
+
+const ProgressCircle = ({ progress = 0.75, size = 40 }) => { 
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    
+    // Ensure progress and size are numbers
+    progress = Number(progress);
+    size = Number(size);
+
+    const angle = progress * 360;
+
+    return (
+        <Box
+            sx={{
+                background: `
+                    radial-gradient(circle at center, ${colors.primary[400]} 55%, transparent 56%),
+                    conic-gradient(${colors.blueAccent[500]} 0deg ${angle}deg, ${colors.greenAccent[500]} ${angle}deg 360deg)`,
+                borderRadius: "50%",
+                width: `${size}px`,
+                height: `${size}px`,
+            }}
+        />
+    );
+};
+
+export default ProgressCircle;
